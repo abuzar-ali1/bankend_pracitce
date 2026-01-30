2ebad6d4c9bc99bd0bc57ca132e1ae9aea405346
abuzar-ali1 <abuzaralixwave@gmail.com>
2026-01-31 00:17:26 +0500

    Connecct the Database

---
+++ src/config/datebase.js
@@ -0,0 +1,19 @@
+import mongoose from mongoose;
+
+
+const connectDB = async () => {
+    try {
+        const connectionInstants = await mongoose.connect
+        (`${process.env.MONGODB_URI}`)
+        console.log(`\n Mongo db Conneccted !! ${connectionInstants.connection.host}`);
+        
+
+    } catch (error) {
+        
+        console.log(error);
+        process.exit(1)
+    }
+}
+
+
+export default connectDB;
