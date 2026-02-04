9eec1dd0b6e273f269c8e701add67f349277d6b9
unknown <abuzar-ali1>
2026-02-04 22:53:12 +0500

    still fixing

--- src/app.js
+++ src/app.js
@@ -3,8 +3,8 @@ import userRouter from "./routes/user.route.js";
 
 const app = express();
 
-app.use(express.json({limit: "16kb"}));
-app.use(express.urlencoded({extended: true, limit: "16kb"}));
+app.use(express.json());
+app.use(express.urlencoded());
 
 
 // A simple test route to check if server is working
@@ -17,5 +17,6 @@ app.get('/', (req, res) => {
 // routes declaration
 
 app.use('/api/v1/users' , userRouter);
+
 
 export default app;   
