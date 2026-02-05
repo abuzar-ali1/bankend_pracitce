568a41762a7adc227e5ef9c3142422b2ec200a21
abuzar-ali1 <abuzaralixwave@gmail.com>
2026-02-05 22:05:18 +0500

    Applying CURD

---
+++ src/controllers/post.controller.js
@@ -0,0 +1,24 @@
+import { Post } from "../models/posts.models";
+
+const registerPost = async (res , req) =>{
+    const {name , description , age} = req.body; 
+    try {
+
+         if(!name || !description || !age){
+                    return res.status(400).json({message : "All fields are Required!"})
+                }        
+                // create  post
+        
+                const post = Post.create({
+                    name,
+                    description,
+                    age
+                })
+                res.status(201).json({
+                    message : "Post regestered succesfully !",
+                    post : {id : user._id , description : post.description , name : post.name , age : post.age}
+                })
+    } catch (error) {
+        
+    }
+}
