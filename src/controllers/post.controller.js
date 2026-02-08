f2c8359bbcc7340c889a6919e5be2047ed97f285
abuzar-ali1 <abuzaralixwave@gmail.com>
2026-02-08 20:59:51 +0500

    Delete Post

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -26,8 +26,41 @@ const registerPost = async (req , res) =>{
 }
 
 
+const deletePost = async (req , res) =>{
+    const {name} = req.body
+    try {
+        if (!name) {
+            return res.status(400).json({message : "Name of post is required to Delete !"})
+        }
+
+        const post = await Post.findOne({
+            name : name
+        })
+
+        if (!post) {
+            return res.status(400).json({
+                message : "Post not Found !"
+            })
+        }
+
+        await Post.deleteOne({name : name});
+        res.status(200).json({
+            message : "Post was deleted successfully !"
+        })
+
+        
+    } catch (error) {
+        res.status(500).json({
+            message : "Internal server error"
+        })
+    }
+
+}
+
+
 
 
 export {
     registerPost,
+    deletePost,
 }
--- src/routes/post.route.js
+++ src/routes/post.route.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import { registerPost } from "../controllers/post.controller.js";
+import { deletePost, registerPost } from "../controllers/post.controller.js";
 
 
 const router = Router();
 
 router.route('/register').post(registerPost);
-
+router.route('/delete').post(deletePost);
 
 export default router;
