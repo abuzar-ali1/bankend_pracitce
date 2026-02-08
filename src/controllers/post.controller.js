955699e58af6c2dcb4ae5d4f5ed082d6b96fbcc2
abuzar-ali1 <abuzaralixwave@gmail.com>
2026-02-08 21:31:52 +0500

    Get All the posts

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -58,9 +58,21 @@ const deletePost = async (req , res) =>{
 }
 
 
+const getPosts = async (req , res) => {
+    try {
+        const posts = await Post.find();
+        res.status(200).json(posts);
+    } catch (error) {
+        res.status(500).json({
+            message : "Internal server error"
+        })
+    }
+}
 
 
 export {
     registerPost,
     deletePost,
+    getPosts,
+
 }
--- src/routes/post.route.js
+++ src/routes/post.route.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { deletePost, registerPost } from "../controllers/post.controller.js";
+import { deletePost, getPosts, registerPost, updatePost } from "../controllers/post.controller.js";
 
 
 const router = Router();
 
 router.route('/register').post(registerPost);
 router.route('/delete').post(deletePost);
-
+router.route('/').get(getPosts);
+// router.route('./update:id').patch(updatePost);
 export default router;
